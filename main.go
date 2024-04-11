f19fb2bccb3e12fc85f689d16b3ee4585c1ceb18
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-04-11 15:49:01 +0300

    Read config

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	URL         string `json:"api_url"`
+	AccessToken string `json:"access_token"`
+	UserName    string `json:"github_username"`
+}
 
 func main() {
-	fmt.Println("Works!")
+	config, err := NewConfig()
+	if err != nil {
+		printError("Error in reading config", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(format(GREEN, config))
+}
+
+func NewConfig() (*Config, error) {
+	data, err := os.ReadFile("config.json")
+	if err != nil {
+		return nil, fmt.Errorf("please create \"config.json\" from \"config.json.tpl\"")
+	}
+
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func printError(text string, err error) {
+	fmt.Fprintf(os.Stderr, format(RED, "%s: %s\n"), text, err)
+}
+
+const (
+	NONE = iota
+	RED
+	GREEN
+	YELLOW
+	BLUE
+	PURPLE
+)
+
+func format(c int, text any) string {
+	const escape = "\x1b"
+
+	color := func(c int) string {
+		var term string
+		if c != NONE {
+			term = "3"
+		}
+
+		return fmt.Sprintf("%s[%s%dm", escape, term, c)
+	}
+
+	return fmt.Sprintf("%s%s%s", color(c), text, color(NONE))
 }
