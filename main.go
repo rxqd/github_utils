3b3728aa13076cdf00d4504fc622275a2e17af4d
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-04-11 18:02:24 +0300

    Fetch repositories from github

--- main.go
+++ main.go
@@ -3,38 +3,142 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
+)
+
+const (
+	APIVersion = "2022-11-28"
+	configFile = "config.json"
+	linksFile  = "links.json"
+	URL        = "https://api.github.com/users/%s/repos"
 )
 
 type Config struct {
-	URL         string `json:"api_url"`
+	URL         string
 	AccessToken string `json:"access_token"`
 	UserName    string `json:"github_username"`
 }
 
+type Repository struct {
+	Name        string `json:"name"`
+	FullName    string `json:"full_name"`
+	Description string `json:"description"`
+	URL         string `json:"html_url"`
+	IsPrivate   bool   `json:"private"`
+	IsFork      bool   `json:"fork"`
+}
+
+var config Config
+
 func main() {
-	config, err := NewConfig()
+	err := InitConfig()
 	if err != nil {
 		printError("Error in reading config", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(format(GREEN, config))
+	fmt.Println("Choose your action")
 }
 
-func NewConfig() (*Config, error) {
-	data, err := os.ReadFile("config.json")
+func FetchAndSaveRepositories() {
+	repos, err := FetchRepositories()
 	if err != nil {
-		return nil, fmt.Errorf("please create \"config.json\" from \"config.json.tpl\"")
+		printError("Error on fetching repos", err)
+		os.Exit(1)
 	}
 
-	var config Config
+	fmt.Println(format(GREEN, "Repositories are fetched successfully"))
+
+	err = SaveRepositories(repos)
+	if err != nil {
+		printError("Error on saving repos", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(format(GREEN, "Repositories are saved successfully"))
+}
+
+func SaveRepositories(repos []Repository) error {
+	file, err := os.Create(linksFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(repos)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FetchRepositories() ([]Repository, error) {
+	var repositories []Repository
+	page := 1
+
+	for {
+		repos, err := doRequest(config.URL, page)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(repos) == 0 {
+			break
+		}
+
+		repositories = append(repositories, repos...)
+		page++
+	}
+
+	return repositories, nil
+}
+
+func doRequest(url string, page int) ([]Repository, error) {
+	client := &http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error on creating request: %w", err)
+	}
+
+	req.Header.Set("X-GitHub-Api-Version", APIVersion)
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("User-Agent", config.UserName)
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", config.AccessToken))
+
+	q := req.URL.Query()
+	q.Add("page", fmt.Sprintf("%d", page))
+	req.URL.RawQuery = q.Encode()
+
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error on making request: %w", err)
+	}
+	defer response.Body.Close()
+
+	var repos []Repository
+	err = json.NewDecoder(response.Body).Decode(&repos)
+
+	return repos, nil
+}
+
+func InitConfig() error {
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return fmt.Errorf("please create \"%s\" from \"%s.tpl\"", configFile, configFile)
+	}
+
 	err = json.Unmarshal(data, &config)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &config, nil
+	config.URL = fmt.Sprintf(URL, config.UserName)
+
+	return nil
 }
 
 func printError(text string, err error) {
